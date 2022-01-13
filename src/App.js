e8a598858029a1d9b7d65c4d1e102ca5cf141328
Barış Sokat <barissokat@outlook.com>
2022-01-13 15:00:43 +0300

    Add multiple slice to store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useSelector } from 'react-redux'
+
+import Auth from './components/Auth'
 import CounterRTK from './components/CounterRTK'
+import Header from './components/Header'
+import UserProfile from './components/UserProfile'
 
 function App () {
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
+
   return (
     <>
       {/* <Counter /> */}
       {/* <CounterConnect /> */}
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <CounterRTK />
     </>
   )
